89f5225c6a47991b5036adaa082427d480f8cf10
Ankita More <ankitabtk86@gmail.com>
2022-07-01 16:31:45 +1000

    Added due date logic, validation function

--- resource/script.js
+++ resource/script.js
@@ -1,5 +1,9 @@
-let taskManager = new TaskManager(0);
+//Object Instance to the class
 
+let taskManager = new TaskManager(0);
+console.log(taskManager);
+
+//Getting the input values
 const form = document.getElementById('form');
 const username = document.getElementById('name');
 const descriptionBox = document.getElementById('description');
@@ -8,59 +12,57 @@ const dueDate = document.getElementById('duedate');
 const validateStatus = document.getElementById('status');
 
 //Due date Logic
-  let ToDate = new Date();
-  dueDateInput = new Date (dueDate.value);
- //console.log(ToDate);
-  //console.log(dueDateInput);
-  
-//Validation Logic
-form.addEventListener('submit',function(e) { 
 
+let today =  new Date();
+ var dd = today.getDate();
+ var mm = today.getMonth()+1; 
+ var yyyy = today.getFullYear();
+    if(mm<10)  {
+         mm='0'+mm;
+     }
+ today = yyyy + "-" + mm + "-" + dd;
+  
+  console.log(today);
+  console.log(dueDate.value);
+  
+
+
+//Validation Logic
+  function validateForm() {
+  
    if (username.value.length <= 8 || username.value === "" ) {
-    e.preventDefault();
+    
     document.getElementById('error-name').innerHTML = "Name should be more then 8 characters";
     }
 
   if ( descriptionBox.value.length <= 15 || descriptionBox.value === "") {
-    e.preventDefault();
+   
     document.getElementById('error-description').innerHTML = "Description should be more then 15 characters";
   }
 
   if (assignedTo.value.length <= 8 || assignedTo.value === "" ) {
-    e.preventDefault();
+   
     document.getElementById('error-assignee').innerHTML= 'Assigned to text should be more then 8 characters';
   }
 
 
-  // if (dueDate.value == '' || Date.parse(dueDateInput) < ToDate.getTime()) {
-
-  //e.preventDefault();
+  if (dueDate.value == '' || dueDate.value < today) {
   
-  //  document.getElementById('error-ddate').innerHTML = "The Date must be Bigger or Equal to today date";
+  document.getElementById('error-ddate').innerHTML = "The Date must be Bigger or Equal to today date";
+  }
 
-  // }
-
+  
   if (validateStatus.selectedIndex == 0) {
-    e.preventDefault();
+   
     document.getElementById('error-status').innerHTML = 'Please select an status option';
   }
 
-taskManager.addTask();
+}
+
+//Adding event listener
+form.addEventListener('submit',function(e) { 
+  e.preventDefault();
+  validateForm();
+  //taskManager.addTask(username.value, descriptionBox.value, assignedTo.value, dueDate.value,  validateStatus.value);
       
 });
-
-//Displaying curret date on page
-function currentDate () {
-  const el = document.getElementById('date');
-  const current= new Date();
-  const day =  current.getDate();
-  const month = current.getMonth()+1;
-  const year = current.getFullYear();
-  
-  // returns the final date with backslash (/) separator
-  const date = `${day}/${month}/${year}`;
-  el.textContent = date;
-
-}
-currentDate();
-
--- resource/taskmanager.js
+++ resource/taskmanager.js
@@ -1,29 +1,29 @@
-const createTaskHtml = (id, username, description, assignedTo, dueDate,  validateStatus) => {
+// const createTaskHtml = (id, username, description, assignedTo, dueDate,  validateStatus) => {
 
-    const newForm = ` 
-    <div class="card"  data-task-id="${id}" style="width: 22rem; margin: 10%; box-shadow: 4px 4px ;">
-        <div class="card-body">
-             <h5 class="card-title">Name:  ${username}</h5>
-              <p class="card-subtitle mb-2 text-muted">Description:  ${description}</p>             
-             <h6 class="card-subtitle mb-2 text-muted" id="assignedToInput">Assignee:  ${assignedTo}</h6>
-             <h6 class="card-subtitle mb-2 text-muted">Due Date:  ${dueDate}</h6>
-             <h6 class="card-subtitle mb-2 text-muted">Status:  ${validateStatus}</h6>
+//     const newForm = ` 
+//     <div class="card"  data-task-id="${id}" style="width: 22rem; margin: 10%; box-shadow: 4px 4px ;">
+//         <div class="card-body">
+//              <h5 class="card-title">Name:  ${username}</h5>
+//               <p class="card-subtitle mb-2 text-muted">Description:  ${description}</p>             
+//              <h6 class="card-subtitle mb-2 text-muted" id="assignedToInput">Assignee:  ${assignedTo}</h6>
+//              <h6 class="card-subtitle mb-2 text-muted">Due Date:  ${dueDate}</h6>
+//              <h6 class="card-subtitle mb-2 text-muted">Status:  ${validateStatus}</h6>
 
-              <div class="row">
-              <div class="col">
-                  <a href="#" class="btn btn-primary">Delete</a>
-               </div>
-               <div class="col">
-                <a href="#" class="btn btn-primary done-button">Mark as done</a>
+//               <div class="row">
+//               <div class="col">
+//                   <a href="#" class="btn btn-primary">Delete</a>
+//                </div>
+//                <div class="col">
+//                 <a href="#" class="btn btn-primary done-button">Mark as done</a>
                 
-               </div>
-               </div>
-        </div>
-    </div>`
+//                </div>
+//                </div>
+//         </div>
+//     </div>`
 
-return newForm;
+// return newForm;
     
-}
+// }
 
 class TaskManager{
 
@@ -33,19 +33,23 @@ class TaskManager{
         
     }
 
-    addTask(username, description, assignedTo, dueDate,  validateStatus) {
+    addTask(username, descriptionBox, assignedTo, dueDate,  validateStatus) {
         const task = {
           id: this.currentId++,
           name: username,
-          description: description,
+          descriptionBox: descriptionBox,
           assignedTo: assignedTo,
           dueDate: dueDate,
           status: validateStatus,
         };
                 
         this.tasks.push(task);
-        console.log(`$(task.id) \n $(task.username)\n $(task.description)\n $(task.assignedTo)\n $(task.dueDate) \n $(task.validateStatus)`);    
+        console.log(`$(task.id) \n $(task.name)\n $(task.descriptionBox)\n $(task.assignedTo)\n $(task.dueDate) \n $(task.status)`);    
         return task
       }
               
 }
+
+
+//localStorage.setItem("task", JSON.stringify(task));
+
