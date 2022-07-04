7e00b3d6bc3cdae362bbe074f4ed9581e40371bb
Ankita More <ankitabtk86@gmail.com>
2022-07-04 12:16:00 +1000

    Added the updated code

--- resource/script.js
+++ resource/script.js
@@ -3,7 +3,7 @@
 let taskManager = new TaskManager(0);
 console.log(taskManager);
 
-//Getting the input values
+//Getting the input values from Form 
 const form = document.getElementById('form');
 const username = document.getElementById('name');
 const descriptionBox = document.getElementById('description');
@@ -12,7 +12,7 @@ const dueDate = document.getElementById('duedate');
 const validateStatus = document.getElementById('status');
 const submitBtn = document.getElementById('submitBTN');
 
-//Due date Logic
+//Selected due date should be greater then current date
 
 let today =  new Date();
  var dd = ("0" + today.getDate()).slice(-2);
@@ -28,9 +28,7 @@ let today =  new Date();
   
 
 
-//Validation Logic
-//form.addEventListener("submit", (event) => {
-
+// Form Validation Logic
   function submitForm (event) {
 
   event.preventDefault(); 
@@ -40,36 +38,40 @@ let today =  new Date();
   if (username.value.length <= 8 || username.value === "" ) {
     
     document.getElementById('error-name').innerHTML = "Name should be more then 8 characters";
+    return false;
     }
 
   if ( descriptionBox.value.length <= 15 || descriptionBox.value === "") {
    
     document.getElementById('error-description').innerHTML = "Description should be more then 15 characters";
+    return false;
   }
 
   if (assignedTo.value.length <= 8 || assignedTo.value === "" ) {
    
     document.getElementById('error-assignee').innerHTML= 'Assigned to text should be more then 8 characters';
+    return false;
   }
 
 
   if (dueDate.value == '' || dueDate.value < today) {
   
   document.getElementById('error-ddate').innerHTML = "The Date must be Bigger or Equal to today date";
+  return false;
   }
 
   
-  if (validateStatus.selectedIndex == 0) {
+  if (validateStatus.selectedIndex === 0) {
    
     document.getElementById('error-status').innerHTML = 'Please select an status option';
+    return false;
   }
-
+ //Adding functions to submit button
   toAddTasks();
 
 };
 
-//);
-
+//Event handler for submit button
 form.addEventListener("submit",submitForm)
 
 
@@ -87,10 +89,13 @@ function currentDate () {
   el.textContent = date;
 
 }
-currentDate();
+currentDate(); //Calling the current date function
 
+//Function to combine all other function 
 function toAddTasks() {
   
   taskManager.addTask(username.value, descriptionBox.value, assignedTo.value, dueDate.value, validateStatus.value);
+  taskManager.render();
        
 }
+
--- resource/taskmanager.js
+++ resource/taskmanager.js
@@ -1,30 +1,37 @@
-// const createTaskHtml = (id, username, description, assignedTo, dueDate,  validateStatus) => {
+//Creating the card format to display on the browser
 
-//     const newForm = ` 
-//     <div class="card"  data-task-id="${id}" style="width: 22rem; margin: 10%; box-shadow: 4px 4px ;">
-//         <div class="card-body">
-//              <h5 class="card-title">Name:  ${username}</h5>
-//               <p class="card-subtitle mb-2 text-muted">Description:  ${description}</p>             
-//              <h6 class="card-subtitle mb-2 text-muted" id="assignedToInput">Assignee:  ${assignedTo}</h6>
-//              <h6 class="card-subtitle mb-2 text-muted">Due Date:  ${dueDate}</h6>
-//              <h6 class="card-subtitle mb-2 text-muted">Status:  ${validateStatus}</h6>
+const createTaskHtml = (id, username, descriptionBox, assignedTo, dueDate,  validateStatus) => {
 
-//               <div class="row">
-//               <div class="col">
-//                   <a href="#" class="btn btn-primary">Delete</a>
-//                </div>
-//                <div class="col">
-//                 <a href="#" class="btn btn-primary done-button">Mark as done</a>
-                
-//                </div>
-//                </div>
-//         </div>
-//     </div>`
+    const newForm = ` 
+    <div class="card" style="width: 20rem; margin: 10%; box-shadow: 4px 4px ;" data-task-id=${id}>
+  <div class="card-body">
+    <h5 class="card-title">Name:  ${username}</h5>
+    <br>
+    <p class="card-subtitle mb-2">Description:  ${descriptionBox}</p>
+    <br>
+    <h6 class="card-subtitle mb-2">Due Date:  ${dueDate}</h6>
+    <br>
+    <h6 class="card-subtitle mb-2" id="assignedToInput">Assignee:  ${assignedTo}</h6>
+    <br>
+    <h6 class="card-subtitle mb-2">Status:  ${validateStatus}</h6>
+    <br>
+     <div class="row">
+      <div class="col">
+        <a href="#" class="btn btn-primary">Delete</a>
+      </div>
+      <div class="col">
+        <a href="#" class="btn btn-primary done-button ml-5">Done</a>
+      </div>
+    </div>
+  </div>
+</div>`
 
-// return newForm;
+
+return newForm;
     
-// }
+}
 
+//Creating class to add task
 class TaskManager{
 
     constructor(currentId =0) {
@@ -32,25 +39,64 @@ class TaskManager{
         this.currentId = currentId;
         
     }
-
+  //Add the task to array
     addTask(username, descriptionBox, assignedTo, dueDate,  validateStatus) {
         const task = {
           id: this.currentId++,
-          name: username,
+          username: username,
           descriptionBox: descriptionBox,
           assignedTo: assignedTo,
           dueDate: dueDate,
-          status: validateStatus,
+          validateStatus: validateStatus,
         };
                 
         this.tasks.push(task);
-        console.log(`$(task.id) \n $(task.name)\n $(task.descriptionBox)\n $(task.assignedTo)\n $(task.dueDate) \n $(task.status)`);    
+        console.log(`$(task.id) \n $(task.username)\n $(task.descriptionBox)\n $(task.assignedTo)\n $(task.dueDate) \n $(task.validateStatus)`);    
         return task
       }
- 
+
+      // Getting the task by id
+
+      getTaskById(taskId) {
+
+        let objTask;
+        for (let i = 0; i < this.tasks.length; i++) {
+          const task = this.tasks[i];
+    
+          if (task.id === taskId) {
+            objTask = task;
+          }
+        }
+        
+        return objTask;
+      }
+
+      //Displaying Task function
+
+      render() {
+
+        const tasksHtmlList = [];
+    
+        for (let i = 0; i < this.tasks.length; i++) {
+          const task = this.tasks[i];
+    
+          const date = new Date(task.dueDate);
+          const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    
+          const taskHtml = createTaskHtml(task.id, task.username, task.descriptionBox, task.assignedTo, formattedDate, task.validateStatus);
+    
+          tasksHtmlList.push(taskHtml);
+        }
+    
+        const tasksHtml = tasksHtmlList.join("\n");
+    
+        const tasksList = document.getElementById('tasksList');
+        tasksList.innerHTML = tasksHtml;
+      }
+
+     
     }
 
 
-     
-//localStorage.setItem("task", JSON.stringify(task));
+
 
